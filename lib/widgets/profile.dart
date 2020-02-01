eaf88ac69a2c049a1e3f7c95fbc765143a3d934e
aadarsh-patel <patelaadarsh0@gmail.com>
2020-02-01 17:09:16 +0530

    update on profile widget

--- lib/widgets/profile.dart
+++ lib/widgets/profile.dart
@@ -4,6 +4,20 @@ class ShadyProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: Image.asset('assets/images/IMG_20151225_1453890.jpg'));
+      padding: EdgeInsets.all(10.0),
+      child: Card(
+        child: Container(
+          width: 400.0,
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Image.asset('assets/images/IMG_20151225_1453890.jpg'),
+              Text('Namaste!\nMy name is Aadarsh Patel and I welcome you to my portfolio.')
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
