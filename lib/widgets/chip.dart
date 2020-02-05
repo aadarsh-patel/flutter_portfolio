5502a1f83641419a456922f5ad83554c4828ef83
aadarsh-patel <patelaadarsh0@gmail.com>
2020-02-06 01:37:31 +0530

    Added chip widget

---
+++ lib/widgets/chip.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ShadyChip extends StatelessWidget {
+  final String title;
+
+  ShadyChip(this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    return Chip(label: Text(title),);
+  }
+}
--- lib/widgets/wrap_list.dart
+++ lib/widgets/wrap_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'list_tile.dart';
+import 'chip.dart';
 
 class ShadyWrapList extends StatelessWidget {
   @override
@@ -8,23 +9,57 @@ class ShadyWrapList extends StatelessWidget {
     MediaQueryData queryData;
     queryData = MediaQuery.of(context);
 
-    return Container(
-      width: queryData.size.width * 0.65,
-      child: Card(
-        elevation: 0.0,
-        color: Colors.transparent,
-        child: Wrap(
-          spacing: 10.0,
-          runSpacing: 3.0,
-          direction: Axis.horizontal,
-          children: <Widget>[
-            ShadyListTile('assets/images/git_logo.png', 'assets/images/git_title.png', 'https://github.com/aadarsh-patel'),
-            ShadyListTile('assets/images/link_logo.png', 'assets/images/link_title.png', 'https://www.linkedin.com/in/aadarsh-patel-634ab1167/'),
-            ShadyListTile('assets/images/code_logo.png', 'assets/images/code_title.png', 'https://www.codechef.com/users/aadarsh_patel'),
-            ShadyListTile('assets/images/stack_logo.png', 'assets/images/stack_title.png', 'https://stackoverflow.com/users/11555932/aadarsh-patel'),
-          ],
+    return Column(
+      children: <Widget>[
+        Container(
+          width: queryData.size.width * 0.65,
+          child: Card(
+            elevation: 0.0,
+            color: Colors.transparent,
+            child: Wrap(
+              spacing: 10.0,
+              runSpacing: 3.0,
+              direction: Axis.horizontal,
+              children: <Widget>[
+                ShadyListTile(
+                    'assets/images/git_logo.png',
+                    'assets/images/git_title.png',
+                    'https://github.com/aadarsh-patel'),
+                ShadyListTile(
+                    'assets/images/link_logo.png',
+                    'assets/images/link_title.png',
+                    'https://www.linkedin.com/in/aadarsh-patel-634ab1167/'),
+                ShadyListTile(
+                    'assets/images/code_logo.png',
+                    'assets/images/code_title.png',
+                    'https://www.codechef.com/users/aadarsh_patel'),
+                ShadyListTile(
+                    'assets/images/stack_logo.png',
+                    'assets/images/stack_title.png',
+                    'https://stackoverflow.com/users/11555932/aadarsh-patel'),
+              ],
+            ),
+          ),
         ),
-      ),
+        Container(
+          width: queryData.size.width * 0.65,
+          child: Card(
+            elevation: 0.0,
+            color: Colors.transparent,
+            child: Wrap(
+              spacing: 10.0,
+              runSpacing: 3.0,
+              direction: Axis.horizontal,
+              children: <Widget>[
+                ShadyChip('App Development'),
+                ShadyChip('Competitive Coding'),
+                ShadyChip('Web Development'),
+                ShadyChip('UI Design'),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
